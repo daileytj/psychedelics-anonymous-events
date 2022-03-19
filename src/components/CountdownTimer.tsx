9ef24648925c7e20ca349044b450c112df54fe28
daileytj <daileytj89@gmail.com>
2022-03-19 17:02:48 -0400

    Update timer expired message

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -80,7 +80,7 @@ export const CountdownTimer: React.FC<CountdownTimerProps> = (
     return (
         <div className={classes.root}>
             {timeUntilEvent.days === 0 && timeUntilEvent.hours === 0 && timeUntilEvent.minutes === 0 && timeUntilEvent.seconds === 0 && (
-            <Typography variant={'h4'} style={{marginTop: 38}}>This Event Is Over</Typography>
+            <Typography variant={'h4'} style={{marginTop: 38}}>This Event Is Currently Running Or It Is Over</Typography>
             )}
             {!(timeUntilEvent.days === 0 && timeUntilEvent.hours === 0 && timeUntilEvent.minutes === 0 && timeUntilEvent.seconds === 0) && (
             <><div className={classes.timeComponent}>
