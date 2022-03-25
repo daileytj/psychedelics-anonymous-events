d0bae42b22833e35caba99c9b5a6834203a3df45
daileytj <daileytj89@gmail.com>
2022-03-24 21:11:55 -0400

    Add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,31 @@ import { NavigationDrawer } from './router/drawer';
 import { MainRouter } from './router/main';
 import { DrawerLayout } from '@brightlayer-ui/react-components';
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseApiKey = process.env.FIREBASE_API_KEY;
+const firebaseAuthDomain = process.env.FIREBASE_AUTH_DOMAIN;
+const firebaseProjectId = process.env.FIREBASE_PROJECT_ID;
+const firebaseStorageBucket = process.env.FIREBASE_STORAGE_BUCKET;
+const firebaseMessagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID;
+const firebaseAppId = process.env.FIREBASE_APP_ID;
+const firebaseMeasurementId = process.env.FIREBASE_MEASUREMENT_ID;
+
+const firebaseConfig = {
+    apiKey: firebaseApiKey,
+    authDomain: firebaseAuthDomain,
+    projectId: firebaseProjectId,
+    storageBucket: firebaseStorageBucket,
+    messagingSenderId: firebaseMessagingSenderId,
+    appId: firebaseAppId,
+    measurementId: firebaseMeasurementId,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+/* eslint-disable */
+const analytics = getAnalytics(firebaseApp);
+/* eslint-enable */
 
 export const App = (): JSX.Element => {
     const [drawerOpen, setDrawerOpen] = useState(false);
